775b70186f556a8a50af4829b06031716d0dfd28
Tomokazu Tantaka <tantaka.tomokazu@icloud.com>
2026-08-28 23:28:52 +0900

    aaa

--- rock5/rock5t.py
+++ rock5/rock5t.py
@@ -24,8 +24,7 @@ BOTTOM = False
 MAIN_WIDTH = 111.2
 MAIN_HEIGHT = 83.0
 
-MAIN_DEPTH = 9.0
-MAIN_DEPTH1 = 9.0
+MAIN_DEPTH1 = 9.6
 MAIN_DEPTH2 = 7.2
 MAIN_DEPTH = MAIN_DEPTH1 + MAIN_DEPTH2
 
@@ -95,8 +94,8 @@ if TOP:
         target=top,
         scale=(41.0, 41.0, MAIN_DEPTH),
         location=(
-            -6.2,
-            -3.8,
+            1.2,
+            -10.6,
             0
         ),
     )
@@ -104,10 +103,10 @@ if TOP:
     ### --------------------------- M.2
     base.cut_cube(
         target=top,
-        scale=(23.0, 7.0, MAIN_DEPTH),
+        scale=(23.3, 7.2, MAIN_DEPTH),
         location=(
-            38.2,
-            -26.0,
+            39.9,
+            -28.6,
             0
         ),
     )
@@ -115,9 +114,9 @@ if TOP:
     ### --------------------------- GPIO
     base.cut_cube(
         target=top,
-        scale=(50.7, 5.1, MAIN_DEPTH),
+        scale=(51.2, 5.6, MAIN_DEPTH),
         location=(
-            23.0,
+            22.3,
             -37.6,
             0
         ),
@@ -128,8 +127,8 @@ if TOP:
         target=top,
         scale=(16.0, 21.0, MAIN_DEPTH),
         location=(
-            -42.3,
-            -18.4,
+            -41.2,
+            -15.4,
             0
         ),
     )
@@ -174,20 +173,20 @@ def cut_Y(scale, x, r=False):
 ### --------------------------- 
 
 H = 15.0
-cut_Y(scale=(10.6, 12.6, H), x=  49.2) # DC
-cut_Y(scale=(14.4, 16.9, H), x=  34.8) # USB1
-cut_Y(scale=(14.4, 16.9, H), x=  16.8) # USB2
-cut_Y(scale=( 5.9, 21.7, H), x=  1.1)  # HDMI1
-cut_Y(scale=( 5.9, 21.7, H), x=-12.8)  # HDMI2
-cut_Y(scale=(16.4, 20.4, H), x=-26.8)  # LAN1
-cut_Y(scale=(16.4, 20.4, H), x=-45.9)  # LAN2
+cut_Y(scale=(10.6, 12.3, H), x= 49.2) # DC
+cut_Y(scale=(14.4, 16.7, H), x= 34.8) # USB1
+cut_Y(scale=(14.4, 16.7, H), x= 16.8) # USB2
+cut_Y(scale=( 6.1, 21.8, H), x=  1.1) # HDMI1
+cut_Y(scale=( 6.1, 21.8, H), x=-12.8) # HDMI2
+cut_Y(scale=(16.4, 20.4, H), x=-26.8) # LAN1
+cut_Y(scale=(16.4, 20.4, H), x=-45.9) # LAN2
 
 ### --------------------------- 
 
-cut_Y(scale=(2.7, 2.6, 3.5), x=-45.0, r=True)  # PW switch
-cut_Y(scale=(6.5, 2.6, 5.2), x=-36.3, r=True)  # Audio Jack
-cut_Y(scale=(9.2, 2.6, 3.0), x=-22.3, r=True)  # SPI
-cut_Y(scale=(2.7, 2.6, 1.5), x=-13.0, r=True)  # USB-C
+cut_Y(scale=(3.0, 2.6, 4.5), x=-44.4, r=True)  # PW switch
+cut_Y(scale=(6.5, 2.6, 7.4), x=-35.9, r=True)  # Audio Jack
+cut_Y(scale=(9.4, 2.6, 5.0), x=-21.8, r=True)  # USB-C
+cut_Y(scale=(2.7, 2.6, 3.0), x=-12.5, r=True)  # SPI
 
 
 #################################################
@@ -205,19 +204,20 @@ def cut_X(scale, y, r=False):
 
 ### --------------------------- 
 
-cut_X(scale=(8.5, 15.7, 5.6), y=-0.4)  # Maskrom BTN
-cut_X(scale=(2.7, 2.6, 3.5), y=20.3)   # HDMI
-cut_X(scale=(2.7, 2.6, 3.5), y=-14.0)  # Recovery BTN
+cut_X(scale=(3.0,  3.0, 4.8), y= 20.3) # Maskrom BTN
+cut_X(scale=(3.0, 15.6, 8.0), y=-0.5)  # HDMI
+cut_X(scale=(3.0,  3.0, 4.8), y=-13.6) # Recovery BTN
 
 ### --------------------------- 
 
-cut_X(scale=(6.7, 15.0, 4.0), y=3.0, r=True)
+#cut_X(scale=(6.7, 15.0, 4.0), y=3.0, r=True)
 
 
 # ##################################################
 if TOP:
     top.location[0] = 8.1
     top.location[1] = 1.3
+    top.location[2] = 0.0
 
 # top.rotation_euler[1] = math.pi
 
