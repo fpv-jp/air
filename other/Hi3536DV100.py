48b93aaa4108ac624dd74380d5cdda541e5352d9
Tomokazu Tantaka <tantaka.tomokazu@icloud.com>
2026-09-03 13:12:34 +0900

    ｗｗ

--- other/Hi3536DV100.py
+++ other/Hi3536DV100.py
@@ -15,8 +15,8 @@ base.init()
 
 # main -----------------------------------
 MAIN_THICKNESS = 1.5
-M4 = 2.0
-M8 = 4.0
+M4_RADIUS = 2.0
+M8_RADIUS = 4.0
 
 MAIN_WIDTH = 119.2
 MAIN_HEIGHT = 47.2
@@ -32,131 +32,118 @@ main = base.create_cube(
     ),
 )
 base.cut_corners(
-    target=main, 
-    width=MAIN_WIDTH, 
-    height=MAIN_HEIGHT, 
-    depth=MAIN_DEPTH + MAIN_THICKNESS, 
-    thickness=MAIN_THICKNESS
+    target=main,
+    width=MAIN_WIDTH,
+    height=MAIN_HEIGHT,
+    depth=MAIN_DEPTH + MAIN_THICKNESS,
+    thickness=MAIN_THICKNESS,
 )
 base.cut_cube(
     target=main,
     scale=(MAIN_WIDTH, MAIN_HEIGHT, MAIN_DEPTH),
 )
 
-X = (MAIN_WIDTH - M4) / 2
-Y = MAIN_HEIGHT / 2 - 17.0 - M4 / 2 - MAIN_THICKNESS
-holes = [(X - 2.2 - MAIN_THICKNESS, Y), (-X + 10.0 + MAIN_THICKNESS / 2, Y)]
-for i, (x, y) in enumerate(holes):
+RING_POSITIONS = [(54.9, 4.1), (-47.9, 4.1)]
+for x, y in RING_POSITIONS:
     base.add_ring(
         target=main,
-        outer_radius=M8,
-        inner_radius=M4,
+        outer_radius=M8_RADIUS,
+        inner_radius=M4_RADIUS,
         depth=MAIN_DEPTH + MAIN_THICKNESS * 2,
         location=(x, y, 0),
     )
 
-H = 2.5
+FLOOR_THICKNESS = 2.5
 base.cut_cube(
     target=main,
-    scale=(MAIN_WIDTH, MAIN_HEIGHT, MAIN_DEPTH-H),
-    location=(0, 0, H/2),
+    scale=(MAIN_WIDTH, MAIN_HEIGHT, MAIN_DEPTH - FLOOR_THICKNESS),
+    location=(0, 0, FLOOR_THICKNESS / 2),
 )
 
 # TOP cut >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-main.location = (0, 0, -MAIN_DEPTH/2 - MAIN_THICKNESS)
+main.location = (0, 0, -MAIN_DEPTH / 2 - MAIN_THICKNESS)
 
 # CPU
-x = 29.0
-y = 29.0
-location = ((MAIN_WIDTH - x) / 2 - 34.3, -(MAIN_HEIGHT - y) / 2 + 2.2, 0)
-base.cut_cube(target=main, scale=(x, y, 10), location=location, rotation=(0, 0, math.radians(1.5)))
+base.cut_cube(
+    target=main,
+    scale=(29.0, 29.0, 10.0),
+    location=(10.8, -6.9, 0),
+    rotation=(0, 0, math.radians(1.5)),
+)
 
 # SATA PWR
-x = 9.5
-y = 16.5
-location = (-(MAIN_WIDTH - x) / 2, -(MAIN_HEIGHT - y) / 2 + 6.2, 0)
-base.cut_cube(target=main, scale=(x, y, 10), location=location)
+base.cut_cube(
+    target=main,
+    scale=(9.5, 16.5, 10.0),
+    location=(-54.9, -9.2, 0),
+)
 
 # SATA DATA
-x = 7.5
-y = 14.8
-location = ((MAIN_WIDTH - x) / 2 - 1.4, -(MAIN_HEIGHT - y) / 2 + 3.6, 0)
-base.cut_cube(target=main, scale=(x, y, 10), location=location)
+base.cut_cube(
+    target=main,
+    scale=(7.5, 14.8, 10.0),
+    location=(54.5, -12.6, 0),
+)
 
 # BATTERY
-x = 23.5
-y = 6.2
-location = ((MAIN_WIDTH - x) / 2 - 10.5, (MAIN_HEIGHT - y) / 2 - 10.15, 0)
-base.cut_cube(target=main, scale=(x, y, 10), location=location)
+base.cut_cube(
+    target=main,
+    scale=(23.5, 6.2, 10.0),
+    location=(37.4, 10.4, 0),
+)
 
 # TOP cut <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-y = 20.25
-pos_z = 18.85
+SIDE_CUT_DEPTH = 20.3
 
-main.location = (MAIN_WIDTH / 2, -MAIN_HEIGHT / 2, -H-MAIN_THICKNESS)
-pos = [
-#   (9.2, 1.8, 11.1),    # DC
-#    (15.0, 15.6, 16.2),  # USB
-    (16.4, 34.2, 12.6),  # ETH
-]
-for i, (x, p, z) in enumerate(pos):
-    base.cut_cube(
-        target=main,
-        scale=(x, y, z),
-        location=(x / 2 + p, -y / 2 + MAIN_THICKNESS, (z - pos_z) / 2+0.5),
-    )
-pos = [
-#   (9.2, 1.8, 11.1),    # DC
-    (15.0, 15.6, 16.2),  # USB
-#    (16.4, 34.2, 12.6),  # ETH
-]
-for i, (x, p, z) in enumerate(pos):
-    base.cut_cube(
-        target=main,
-        scale=(x, y, z),
-        location=(x / 2 + p, -y / 2 + MAIN_THICKNESS, (z - pos_z) / 2+0.75),
-    )
+main.location = (59.6, -23.6, -FLOOR_THICKNESS - MAIN_THICKNESS)
+
+# ETH
+base.cut_cube(
+    target=main,
+    scale=(16.4, SIDE_CUT_DEPTH, 12.6),
+    location=(42.4, -8.6, -2.6),
+)
+
+# USB
+base.cut_cube(
+    target=main,
+    scale=(15.0, SIDE_CUT_DEPTH, 16.2),
+    location=(23.1, -8.6, -0.6),
+)
+
 # DC
 base.cut_cylinder(
     target=main,
     radius=4.2,
-    depth=MAIN_THICKNESS*3,
-    location=(4.2+3.0, 0, -2.0),
+    depth=MAIN_THICKNESS * 3,
+    location=(7.2, 0, -2.0),
     rotation=(math.pi / 2, 0, 0),
 )
 
-main.location = (-MAIN_WIDTH / 2, -MAIN_HEIGHT / 2, -H-MAIN_THICKNESS)
-pos = [
-#    (15.2, 46.5, 6.2),  # HDMI
-    (30.9, 9.7, 12.7),  # DVI
-#   (5.4, 1.7, 5.7),    # AUX
-]
-for i, (x, p, z) in enumerate(pos):
-    base.cut_cube(
-        target=main,
-        scale=(x, y, z),
-        location=(-x / 2 - p, y / 2, (z - pos_z) / 2),
-    )
-pos = [
-    (15.2, 46.5, 6.2),  # HDMI
-#    (30.9, 9.7, 12.7),  # DVI
-#   (5.4, 1.7, 5.7),    # AUX
-]
-for i, (x, p, z) in enumerate(pos):
-    base.cut_cube(
-        target=main,
-        scale=(x, y, z),
-        location=(-x / 2 - p, y / 2, (z - pos_z) / 2+ 0.5),
-    )
-    
+main.location = (-59.6, -23.6, -FLOOR_THICKNESS - MAIN_THICKNESS)
+
+# DVI
+base.cut_cube(
+    target=main,
+    scale=(30.9, SIDE_CUT_DEPTH, 12.7),
+    location=(-25.2, 10.1, -3.1),
+)
+
+# HDMI
+base.cut_cube(
+    target=main,
+    scale=(15.2, SIDE_CUT_DEPTH, 6.2),
+    location=(-54.1, 10.1, -5.8),
+)
+
 # AUX
 base.cut_cylinder(
     target=main,
     radius=2.7,
-    depth=MAIN_THICKNESS*3,
-    location=(-4.3, 0, -6),
+    depth=MAIN_THICKNESS * 3,
+    location=(-4.3, 0, -6.0),
     rotation=(math.pi / 2, 0, 0),
 )
 
